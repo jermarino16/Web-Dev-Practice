49b4ee3848bc1be48f9becc1907728f06b5013d5
Jeremy Marino <jermarino16@gmail.com>
2020-01-28 08:24:36 -0800

    Factorial example

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -1,8 +1,19 @@
-function square(num){
-	// console.log(num*num);
-	return num*num;
+// function square(num){
+// 	// console.log(num*num);
+// 	return num*num;
+// }
+
+// var num = prompt("What do you want to square? ");
+
+// console.log("The square is: " + square(num));
+
+function factorial(num){
+	while (num != 1){
+		return num * factorial(num-1)
+	}
+	return 1
 }
 
-var num = prompt("What do you want to square? ");
+var num = prompt("What do you want to factorial? ");
 
-console.log("The square is: " + square(num));
+console.log(factorial(num))
