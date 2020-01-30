f689ce9231e9ca253d2c8018ef19c64453c1fcaa
Jeremy Marino <jermarino16@gmail.com>
2020-01-29 18:12:43 -0800

    got both scores working

--- DOM/keeper.js
+++ DOM/keeper.js
@@ -6,9 +6,24 @@ var p2Text = document.getElementById("p2-score");
 var p2Button = document.getElementById("p2");
 var p2Score = 0;
 
+var resetButton = document.getElementById("reset");
+
+
 p1Button.addEventListener("click", function(){
     p1Score++;
     p1Text.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+    p2Score++;
+    p2Text.textContent = p2Score;
+});
+
+resetButton.addEventListener("click", function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Text.textContent = p1Score;
+    p2Text.textContent = p2Score;
 });
 // p2Button.addEventListener("click", updateScore(p2Score + 1, "player2"));
 
