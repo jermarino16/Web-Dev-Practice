6787b4667f55941c1623929f7538a757ce589485
Jeremy Marino <jermarino16@gmail.com>
2020-02-03 12:42:50 -0800

    events now disappear after clicking the X 'delete button'

--- Projects/Todo_List/Assets/js/todos.js
+++ Projects/Todo_List/Assets/js/todos.js
@@ -3,6 +3,10 @@ $("li").click(function(){
     $(this).toggleClass("done");
 });
 
-$("li span").click(function(){
-    $(this).parents("li").fadeOut();
+$("li span").click(function(event){
+    $(this).parent().fadeOut(500, function()
+    {
+        this.remove();
+    });
+    event.stopPropagation();
 })
