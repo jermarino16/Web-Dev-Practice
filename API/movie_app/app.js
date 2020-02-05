d28e5563e454b76a3d511219007dc02f3bf8db4b
Jeremy Marino <jermarino16@gmail.com>
2020-02-05 05:32:56 +0000

    working with ejs files and rendering

--- API/movie_app/app.js
+++ API/movie_app/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs"); //do this so we dont have to call files "results.ejs" and can type "results" instead
 
 // route for "/"
 app.get("/", function(req, res){
@@ -12,7 +13,9 @@ app.get("/results", function(req, res){
 	request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
 		if(!error && response.statusCode == 200){
 			var results = JSON.parse(body);
-			res.send(results);
+			// res.send(results.Search[0].Title);
+			console.log(results.Search[0].Title);
+			res.render("results");
 		}
 	})
 })
