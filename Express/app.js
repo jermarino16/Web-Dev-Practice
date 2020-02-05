e7b15225a7c5e0d99345f852c0f24c16ba8ccfee
Jeremy Marino <jermarino16@gmail.com>
2020-02-05 00:57:37 +0000

    practice messing with params

--- Express/app.js
+++ Express/app.js
@@ -16,6 +16,21 @@ app.get("/dog", function(request, response){
 	response.send("WOOF");
 });
 
+// route for a subreddit where ":anything" is a param and is anything -- so /r/test would work
+app.get("/r/:subreddit", function(request,response){
+	// console.log(request.params)
+	var subredditName = request.params.subreddit;
+	response.send("Welcome to the " + subredditName + " page" );
+	
+})
+
+// route for a subreddit comments
+app.get("/r/:subreddit/comments/:id/:title", function(request,response){
+	console.log(request.params)
+	response.send("Hey you found a subreddit!");
+})
+
+
 //route for "*"
 app.get("*", function(request, response){
 	response.send("You are a *");
